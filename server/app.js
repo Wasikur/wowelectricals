8d2d35b5bf0ffb58be46a1a9aa8fe6aaca6a09fa
Wasikur <69371416+Wasikur@users.noreply.github.com>
2023-11-03 23:13:31 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,27 +1,32 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors"); // Import the 'cors' package
 const app = express();
 
-// file secure using dotenv -
+// File secure using dotenv -
 dotenv.config({ path: "./config.env" });
 
-// CORS Policy error
-app.options("*", require("./cors/cors"));
-app.use(require("./cors/cors"));
+// Apply CORS middleware
+app.use(cors({
+  origin: "https://wowelectricals-frontend.vercel.app", // Specify the allowed origin
+  methods: ["POST", "GET"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+}));
 
 app.use(express.json());
 
-// database connection -
+// Database connection -
 require("./db/conn");
 
 // Router files
 app.use(require("./router/auth"));
 
-// import PORT variable data -
+// Import PORT variable data -
 const PORT = process.env.PORT;
 
-// to check server work or not
+// To check if the server is running or not
 app.listen(PORT, () => {
-  console.log(`server is running at port no ${PORT}`);
+  console.log(`Server is running at port no ${PORT}`);
 });
