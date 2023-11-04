8e04ad3cef317c1a9d2952b7a262c3edefe861bb
Wasikur <69371416+Wasikur@users.noreply.github.com>
2023-11-04 09:33:26 +0530

    Update cors.js

--- server/cors/cors.js
+++ server/cors/cors.js
@@ -1,31 +1,11 @@
-// const cors = require("cors");
-
-// const corsOptions = {
-//   origin: ["https://wowelectricals-frontend.vercel.app"],
-//   methods: ["POST", "GET"],
-//   credentials: true,
-//   optionsSuccessStatus: 200, //some legacy browsers choke on 204
-// };
-
-// module.exports = cors(corsOptions);
-
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "https://wowelectricals-frontend.vercel.app", // Specify the allowed origin
+  origin: ["https://wowelectricals-frontend.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true,
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200, //some legacy browsers choke on 204
 };
 
-const corsMiddleware = cors(corsOptions);
-
-module.exports = (req, res, next) => {
-  corsMiddleware(req, res, (error) => {
-    if (error) {
-      return next(error);
-    }
-    next();
-  });
-};
+module.exports = cors(corsOptions);
 
